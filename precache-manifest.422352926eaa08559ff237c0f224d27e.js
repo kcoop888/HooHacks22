43cf204c4126adc331e8d91b3a125ff90ddfb31f
Kevin Cooper <kac4bh@virginia.edu>
2022-03-27 06:00:06 -0400

    Updates

---
+++ precache-manifest.422352926eaa08559ff237c0f224d27e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "580bbe95766f46715959",
+    "url": "/HooSpots/static/css/main.a019909e.chunk.css"
+  },
+  {
+    "revision": "580bbe95766f46715959",
+    "url": "/HooSpots/static/js/main.179a7fa8.chunk.js"
+  },
+  {
+    "revision": "557e7f670478256b98be",
+    "url": "/HooSpots/static/js/runtime~main.44f6960b.js"
+  },
+  {
+    "revision": "ccca3f366e7d99c0ac9d",
+    "url": "/HooSpots/static/js/2.f6cfaf0c.chunk.js"
+  },
+  {
+    "revision": "c832614f3909c6f83d95",
+    "url": "/HooSpots/static/js/3.41c7b25b.chunk.js"
+  },
+  {
+    "revision": "6266e09aca682b9a10daa0dee75030c2",
+    "url": "/HooSpots/static/media/uva2.6266e09a.png"
+  },
+  {
+    "revision": "5f35422313bf5c90375539060d1edd17",
+    "url": "/HooSpots/index.html"
+  }
+];
